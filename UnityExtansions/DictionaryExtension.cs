c101d210e2820c0163e4d6558158775614df64aa
Alexander <alexander.tkachenko@synebo.io>
2021-11-05 04:29:05 +0200

    Added List Extensions and Dictionary Extensions

---
+++ UnityExtansions/DictionaryExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Collections.Generic
+{
+    public static class DictionaryExtension
+    {
+        public static bool IsEmpty<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return dictionary == null || dictionary.Count == 0;
+        }
+
+        public static bool IsNotEmpty<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return dictionary.IsEmpty() == false;
+        }
+    }
+}
---
+++ UnityExtansions/ListExtension.cs
@@ -0,0 +1,53 @@
+﻿namespace System.Collections.Generic
+{
+    public static class ListExtension
+    {
+        private static readonly string _listEmptyErrorMessage = "Error: List is empty!";
+
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            return list == null || list.Count == 0;
+        }
+
+        public static bool IsNotEmpty<T>(this List<T> list)
+        {
+            return list.IsEmpty() == false;
+        }
+
+        public static T FirstIndexOf<T>(this List<T> list)
+        {
+            if (list.IsNotEmpty())
+            {
+                return list[0];
+            }
+            else
+            {
+                throw new NullReferenceException(ListExtension._listEmptyErrorMessage);
+            }
+        }
+
+        public static T LastIndexOf<T>(this List<T> list)
+        {
+            if (list.IsNotEmpty())
+            {
+                return list[list.Count - 1];
+            }
+            else
+            {
+                throw new NullReferenceException(ListExtension._listEmptyErrorMessage);
+            }
+        }
+
+        public static T RandomIndexOf<T>(this List<T> list)
+        {
+            if (list.IsNotEmpty())
+            {
+                return list[UnityEngine.Random.Range(0, list.Count)];
+            }
+            else
+            {
+                throw new NullReferenceException(ListExtension._listEmptyErrorMessage);
+            }
+        }
+    }
+}
